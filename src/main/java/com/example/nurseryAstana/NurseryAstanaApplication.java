dbe4049e2245addb59d2f1859d516fd0c5029ef9
neosomee <daniildaresin@gmail.com>
2026-03-18 16:02:45 +0300

    init Start project nurseryAstana

---
+++ src/main/java/com/example/nurseryAstana/NurseryAstanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.nurseryAstana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NurseryAstanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NurseryAstanaApplication.class, args);
+	}
+
+}
