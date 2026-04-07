97094c8fa8ae78dcfbf9010e6bb263b5d7cb549d
Titov Ilya <titov1608@gmail.com>
2026-04-07 16:24:30 +0300

    Создал контроллер AnimalController, где реализовал CRUD запросы

--- src/main/java/com/example/nurseryAstana/service/AnimalService.java
+++ src/main/java/com/example/nurseryAstana/service/AnimalService.java
@@ -3,7 +3,6 @@ package com.example.nurseryAstana.service;
 import com.example.nurseryAstana.model.Animal;
 import com.example.nurseryAstana.repository.AnimalRepository;
 import lombok.AllArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
