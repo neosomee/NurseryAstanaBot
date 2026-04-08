da77bd338fae4791b35f6efb20409d9538245964
Titov Ilya <titov1608@gmail.com>
2026-04-08 19:33:19 +0300

    Исправил статусы(Сделал так что бы была VARCHAR в бд) в Animal

--- src/main/java/com/example/nurseryAstana/model/Animal.java
+++ src/main/java/com/example/nurseryAstana/model/Animal.java
@@ -18,11 +18,14 @@ public class Animal {
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
+    @Enumerated(EnumType.STRING)
     private Species species;
     private String name;
     private int age;
     private String breed;
     private String description;
+
+    @Enumerated(EnumType.STRING)
     private AnimalStatus status;
 
     @Column(name = "created_at", insertable = false, updatable = false)
