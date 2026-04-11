403a67dabb921e0813f4cebdc166eb5f3e421eae
Enotiks-f <titov1608@gmail.com>
2026-04-11 17:37:01 +0300

    Revert "Сделал Dto "

--- src/main/java/com/example/nurseryAstana/repository/AnimalRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.nurseryAstana.repository;
-
-import com.example.nurseryAstana.model.Animal;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AnimalRepository extends JpaRepository<Animal, Long> {
-}
